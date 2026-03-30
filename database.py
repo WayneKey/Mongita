842e4ce91ebbbca5c3039719599e315a6afc4c11
WayneKey <waynecheangk8@gmail.com>
2026-03-30 22:48:55 +0000

    First

--- database.py
+++ database.py
@@ -1,28 +1,24 @@
 import os
+import shutil
+from mongita import MongitaClientDisk
 
-from peewee import IntegerField, Model, SqliteDatabase, TextField
+client = None
+db = None
+pets = None
 
-database = SqliteDatabase(None)
-
-class BaseModel(Model):
-    class Meta:
-        database = database
-
-class Pet(BaseModel):
-    name = TextField(null=False)
-    type = TextField(null=False)
-    age = IntegerField(default=0)
-
-def initialize(database_file):
-    if not database.is_closed():
-        database.close()
-    database.init(database_file)
-    database.connect(reuse_if_open=True)
-    database.create_tables([Pet])
+def initialize(database_dir="pets_db"):
+    global client, db, pets
+    client = MongitaClientDisk(host=database_dir)
+    db = client["pet_app"]
+    pets = db["pets"]
 
 def close_connection():
-    if not database.is_closed():
-        database.close()
+    global client, db, pets
+    if client is not None:
+        client.close()
+    client = None
+    db = None
+    pets = None
 
 def _normalize_age(value):
     try:
@@ -30,16 +26,20 @@ def _normalize_age(value):
     except Exception:
         return 0
 
-def pet_to_dict(pet):
-    return {"id": pet.id, "name": pet.name, "type": pet.type, "age": pet.age}
-
+def pet_to_dict(doc):
+    return {
+        "id": doc["_id"],
+        "name": doc["name"],
+        "type": doc["type"],
+        "age": doc["age"],
+    }
 
 def get_pets():
-    query = Pet.select().order_by(Pet.name)
+    query=list(pets.find({}))
     return [pet_to_dict(pet) for pet in query]
 
 def get_pet(id):
-    pet = Pet.get_or_none(Pet.id == int(id))
+    pet = pets.find_one({"_id":id})
     if pet is None:
         return None
     return pet_to_dict(pet)
@@ -49,34 +49,34 @@ def create_pet(data):
         raise Exception("Hey! Pet doesn't have name.")
     if data["name"].strip()=="":
         raise Exception("Hey! Pet doesn't have name.")
-    pet = Pet.create(
-        name=(data.get("name") or "").strip(),
-        type=(data.get("type") or "").strip(),
-        age=_normalize_age(data.get("age")),
-    )
+    ###################################################
+    pet = pets.insert_one({
+        "name":(data.get("name") or "").strip(),
+        "type":(data.get("type") or "").strip(),
+        "age":_normalize_age(data.get("age")),
+    })
     return pet.id
 
 def delete_pet(id):
-    Pet.delete().where(Pet.id == int(id)).execute()
-
+    pets.delete_one({"_id":id})
 
 def update_pet(id, data):
     if "name" not in data:
         raise Exception("Hey! Pet doesn't have name.")
     if data["name"].strip()=="":
         raise Exception("Hey! Pet doesn't have name.")
-    Pet.update(
-        name=(data.get("name") or "").strip(),
-        type=(data.get("type") or "").strip(),
-        age=_normalize_age(data.get("age")),
-    ).where(Pet.id == int(id)).execute()
+    ###################################################
+    pets.update_one({"_id":id},{
+        "name":(data.get("name") or "").strip(),
+        "type":(data.get("type") or "").strip(),
+        "age":_normalize_age(data.get("age")),
+    })
 
-def setup_test_database(db_file="test_pets.db"):
+def setup_test_database(db_file="test_mongita"):
     close_connection()
-    try:
-        os.remove(db_file)
-    except FileNotFoundError:
-        pass
+
+    if os.path.exists(db_file):
+        shutil.rmtree(db_file)
 
     initialize(db_file)
 
@@ -91,16 +91,14 @@ def setup_test_database(db_file="test_pets.db"):
 
     assert len(get_pets()) == 4
 
-
 def test_get_pets():
-    pets = get_pets()
-    assert type(pets) is list
-    assert len(pets) >= 1
-    assert type(pets[0]) is dict
+    petAll = get_pets()
+    assert type(petAll) is list
+    assert len(petAll) >= 1
+    assert type(petAll[0]) is dict
     for key in ["id", "name", "type", "age"]:
         assert key in pets[0]
     assert type(pets[0]["name"]) is str
-
 
 def test_create_pet_and_get_pet():
     new_id = create_pet({"name": "walter", "age": "2", "type": "mouse"})
@@ -110,7 +108,6 @@ def test_create_pet_and_get_pet():
     assert pet["age"] == 2
     assert pet["type"] == "mouse"
 
-
 def test_update_pet():
     new_id = create_pet({"name": "temp", "age": 1, "type": "cat"})
     update_pet(new_id, {"name": "updated", "age": "8", "type": "dog"})
@@ -119,7 +116,6 @@ def test_update_pet():
     assert pet["name"] == "updated"
     assert pet["age"] == 8
     assert pet["type"] == "dog"
-
 
 def test_delete_pet():
     new_id = create_pet({"name": "delete_me", "age": 3, "type": "fish"})
